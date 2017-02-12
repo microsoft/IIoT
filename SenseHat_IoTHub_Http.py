6f7e95a16ea403eb9c626389ca10369e60e66ce2
Kevin Hilscher <kevin.hilscher@gmail.com>
2017-02-11 17:15:30 -0700

    Update SenseHat_IoTHub_Http.py
    Made compatible with Python 2.7 and 3.5.

--- SenseHat_IoTHub_Http.py
+++ SenseHat_IoTHub_Http.py
@@ -1,5 +1,7 @@
 """
 Sample code demonstrating Sense Hat running on Raspian and Raspberry Pi 3 sending temperature data to Azure IoT Hub using Azure IoT Hub REST API.
+
+Test with Python 2.7 and 3.5
 """
 
 import base64
@@ -9,10 +11,11 @@ import time
 import requests
 import urllib
 import time
+import sys
 
 #If using emulator, uncomment 'from sense_emu import SenseHat ' and comment out 'from sense_hat import SenseHat'
-from sense_hat import SenseHat #if using a physical SenseHat attached to Raspi
-# from sense_emu import SenseHat #if using a SenseHat Emulator
+#from sense_hat import SenseHat #if using a physical SenseHat attached to Raspi
+from sense_emu import SenseHat #if using a SenseHat Emulator
 
 class D2CMsgSender:
     
@@ -36,11 +39,20 @@ class D2CMsgSender:
         expiryTime = self._buildExpiryOn()
         toSign = '%s\n%s' % (targetUri, expiryTime)
         key = base64.b64decode(self.keyValue.encode('utf-8'))
-        signature = urllib.quote(
-            base64.b64encode(
-                hmac.HMAC(key, toSign.encode('utf-8'), hashlib.sha256).digest()
-            )
-        ).replace('/', '%2F')
+
+        if sys.version_info[0] < 3:
+            signature = urllib.quote(
+                base64.b64encode(
+                    hmac.HMAC(key, toSign.encode('utf-8'), hashlib.sha256).digest()
+                )
+            ).replace('/', '%2F')
+        else:
+            signature = urllib.parse.quote(
+                base64.b64encode(
+                    hmac.HMAC(key, toSign.encode('utf-8'), hashlib.sha256).digest()
+                )
+            ).replace('/', '%2F')
+
         return self.TOKEN_FORMAT % (signature, expiryTime, self.keyName, targetUri)
     
     def sendD2CMsg(self, deviceId, message):
@@ -49,10 +61,11 @@ class D2CMsgSender:
         r = requests.post(url, headers={'Authorization': sasToken}, data=message)
         return r.text, r.status_code
     
-#if __name__ == '__main__':
+if __name__ == '__main__':
 
     #Replace <iot_hub_name> and <sas_key> with values obtained from Azure Portal or Device Explorer
-    connectionString = 'HostName=<iot_hub_name>.azure-devices.net;SharedAccessKeyName=device;SharedAccessKey=<sas_key>'
+    #Ensure you use IoT Hub owner connection string, not a device connection string here.
+    connectionString = 'HostName=KHIoTHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=PUbIrbAmPvgcc1kIAW5bDlNQG6/Y7zJSoi6qSiNpcio='
     
     #Replace with deviceId registered with your IoT Hub.
     deviceId = 'Device1'
@@ -64,6 +77,6 @@ class D2CMsgSender:
         #Send temperature from the Sense HAT
         #Refer to Sense HAT API to send other Sense HAT sensor data https://pythonhosted.org/sense-hat/api/
         message = str(sense.temp) 
-        print 'Sending message... ' + message
-        print d2cMsgSender.sendD2CMsg(deviceId, message)
+        print('Sending message... ' + message)
+        print(d2cMsgSender.sendD2CMsg(deviceId, message))
         time.sleep(5) # 5 second delay
